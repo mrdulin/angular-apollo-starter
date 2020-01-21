3da13a2ac8f0c83f58e3da8947abb22a2ee100fd
mrdulin <novaline@qq.com>
2020-01-21 14:25:08 +0800

    improve sample

--- src/stackoverflow/59790114/closures.test.js
+++ src/stackoverflow/59790114/closures.test.js
@@ -1,8 +1,7 @@
 const closures = require('./closures');
 
-describe('Closure', () => {
+fdescribe('Closure', () => {
   let closure;
-  let runOnce;
   const hello = () => console.log('hello');
 
   beforeEach(() => {
@@ -12,14 +11,20 @@ describe('Closure', () => {
   describe('after', () => {
     beforeEach(() => {
       spyOn(console, 'log');
-      runOnce = closure.after(1, hello);
     });
 
     it('executes callback after called first x times', () => {
+      const runOnce = closure.after(1, hello);
       const first = runOnce();
       expect(first).toEqual(undefined);
       expect(console.log).toHaveBeenCalledWith('hello');
       expect(console.log.calls.count()).toEqual(1);
+    });
+    it('should not call calback if counter is not equal with r', () => {
+      const runOnce = closure.after(2, hello);
+      const first = runOnce();
+      expect(first).toEqual(undefined);
+      expect(console.log).not.toHaveBeenCalled();
     });
   });
 });
